ef95e0c820e403c07ebb3626938454ad9f13594d
whille <whille@163.com>
2026-06-01 18:13:22 +0800

    upd

--- dist/stubs/config.d.ts
+++
@@ -1,3 +0,0 @@
-import type { LoomerConfig } from "../types/web.js";
-export declare function createDefaultConfig(overrides?: Partial<LoomerConfig>): LoomerConfig;
-//# sourceMappingURL=config.d.ts.map
