1f2dc7017b9f21f3de5480d7305a0401397af329
whille <whille@163.com>
2026-06-01 10:49:19 +0800

    chore: 从 git 移除 dist/ track，.gitignore 生效

--- dist/cli.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { Command } from "commander";
-import { LoomerClient } from "./cli-client.js";
-import { LoomerConfig } from "./config.js";
-export declare function createProgram(clientFactory?: (config: LoomerConfig) => LoomerClient): Command;
-//# sourceMappingURL=cli.d.ts.map
